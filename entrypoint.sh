79893730e7bc84f988ff8cb6cab78337080cec08
Aviyam Fischer <aviyam@gmail.com>
2019-10-27 15:46:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 
 echo "------ Writing config file ------"
 
-echo exporter.port=$EXPORTER_PORT									> config.properties
+echo exporter.port=$EXPORTER_PORT									                  > config.properties
 echo exporter.metrix.expire.seconds=$EXPORTER_METRIX_EXPIRE_SECONDS	>> config.properties
-echo kafka.consumer.topics=$KAFKA_CONSUMER_TOPICS					>> config.properties
-echo kafka.consumer.remove.prefix=$KAFKA_CONSUMER_REMOVE_PREFIX		>> config.properties
-echo consul.server.url=$CONSUL_SERVER_URL							>> config.properties
-echo consul.kafka.servicename=$CONSUL_KAFKA_SERVICENAME				>> config.properties
-echo bootstrap.servers=$BOOTSTRAP_SERVERS							>> config.properties
-echo group.id=$GROUP_ID												>> config.properties
+echo kafka.consumer.topics=$KAFKA_CONSUMER_TOPICS					          >> config.properties
+echo kafka.consumer.remove.prefix=$KAFKA_CONSUMER_REMOVE_PREFIX		  >> config.properties
+echo consul.server.url=$CONSUL_SERVER_URL			               				>> config.properties
+echo consul.kafka.servicename=$CONSUL_KAFKA_SERVICENAME			      	>> config.properties
+echo bootstrap.servers=$BOOTSTRAP_SERVERS							              >> config.properties
+echo group.id=$GROUP_ID										                      		>> config.properties
 
 
 echo exporter.port=$EXPORTER_PORT
